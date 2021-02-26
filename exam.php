45e5c9b9b0d2995b8c3b7013d64c3e5ac750818e
kumaravel3657 <58325275+kumaravel3657@users.noreply.github.com>
2021-02-26 09:54:02 +0530

    Create exam.php
    First-round file

---
+++ exam.php
@@ -0,0 +1,26 @@
+<?php
+
+$input = "WATER";
+
+$input_length = strlen($input);
+$get_half = $input_length / 2; 
+$a = ($input_length % 2 == 0) ? substr($input, (floor($get_half) - 1), 2) : substr($input, $get_half, 1);
+echo $a."<br>";
+$words = explode($a, $input);
+$final = array_reverse($words);
+foreach($final as $value){
+    
+    $strlen = strlen( $value );
+        for( $i = 0; $i <= $strlen; $i++ ) {
+            $a.=  $char = substr( $value, $i, 1 );
+            echo $a;
+            echo "<br>";
+            
+
+    }
+}
+
+
+
+
+?>
